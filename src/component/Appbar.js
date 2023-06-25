dd86f161e585a75426df1305dfe7e51ac97dd0f3
SOUVIGHOSH <souvik.personal.email@gmail.com>
2023-06-25 23:11:42 +0530

    first commit

---
+++ src/component/Appbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Appbar = () => {
+  return (
+    <div className="appbar">
+      <div className="logo">Tale</div>
+    </div>
+  );
+};
+
+export default Appbar;
---
+++ src/component/Post.js
@@ -0,0 +1,9 @@
+const Post = () => {
+  return (
+    <div className="post-container">
+      <div className="post-container__input">Tell your story</div>
+    </div>
+  );
+};
+
+export default Post;
